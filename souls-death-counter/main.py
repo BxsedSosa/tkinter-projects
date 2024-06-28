f45fab1314c06dfc0a595dc31ee43b504f648ba8
BxsedSosa <donovanxabad@gmail.com>
2024-06-28 13:37:39 -0700

    Completed logic for saving which boss youre on before exiting

--- souls-death-counter/main.py
+++ souls-death-counter/main.py
@@ -46,6 +46,16 @@ def update_existing_data(name: str, death_counter: int, key_idx: int) -> None:
         file.truncate()
 
 
+def update_last_used(name: str, death_counter: int):
+    with open("data.json", "r+") as file:
+        file_data = json.load(file)
+
+        file_data["last_used"] = {name: {"death": death_counter}}
+        file.seek(0)
+        json.dump(file_data, file, indent=4)
+        file.truncate()
+
+
 def read_from_json() -> dict:
     with open("data.json", "r") as file:
         data = json.load(file)
@@ -60,10 +70,16 @@ def search_name() -> None:
     boss_data = read_from_json()
 
     for idx, data in enumerate(boss_data["boss"]):
+        x = list(data.keys())
+        if x[0] == name and name == current_name:
+            update_existing_data(current_name, current_death_count, idx)
+            change_gui_data(current_name, current_death_count)
+            return
+
         if name in data:
-            change_gui_data(name, data[name]["death"])
             if current_name in data:
                 update_existing_data(current_name, current_death_count, idx)
+            change_gui_data(name, data[name]["death"])
             return
 
     save_new_data(name, 0)
@@ -75,11 +91,25 @@ def change_gui_data(name: str, curr_death_count: int) -> None:
     death_count.set(curr_death_count)
 
 
+def starting_data() -> None:
+    data = read_from_json()
+    name = list(data["last_used"].keys())
+    death = data["last_used"][name[0]]["death"]
+
+    boss_name.set(name[0])
+    death_count.set(death)
+
+
 def on_close() -> None:
     name = boss_name.get()
     counter = death_count.get()
-    data = {name: {"death": counter}}
+    boss_data = read_from_json()
 
+    for idx, data in enumerate(boss_data["boss"]):
+        if name in data:
+            update_existing_data(name, counter, idx)
+
+    update_last_used(name, counter)
     root.destroy()
 
 
@@ -94,13 +124,13 @@ game_picture = ttk.Label(pane, image=img).grid(
 boss_title = ttk.Label(pane, textvariable=boss_name).grid(
     row=2, column=0, padx=30, pady=10, sticky="ws"
 )
-increment_count = ttk.Button(pane, text="up", command=increase_counter).grid(
+increment_count = ttk.Button(pane, text="+", command=increase_counter).grid(
     row=2, column=1, padx=30, sticky="es"
 )
 counter = ttk.Label(pane, textvariable=death_count, font=("Arial", 40)).grid(
     row=3, column=0, padx=30, sticky="w"
 )
-decrement_count = ttk.Button(pane, text="dw", command=decrase_counter).grid(
+decrement_count = ttk.Button(pane, text="-", command=decrase_counter).grid(
     row=3, column=1, padx=30, sticky="ne"
 )
 boss_search_text = ttk.Label(pane, text="Search:").grid(
@@ -113,7 +143,6 @@ enter_search = ttk.Button(pane, text="Enter", command=search_name).grid(
     row=5, column=1, pady=10, padx=30, sticky="w"
 )
 
-boss_name.set("Boss Name")
-
+starting_data()
 root.protocol("WM_DELETE_WINDOW", on_close)
 root.mainloop()
